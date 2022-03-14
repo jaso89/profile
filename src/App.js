aa2747a4d2e4c0498e679b34e20df37f1961c4f4
jaso89 <soronen.jani@gmail.com>
2022-03-14 10:32:37 +0200

    pushing content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <header className="App-header">
+        Hello World
+      </header>
     </div>
   );
 }
